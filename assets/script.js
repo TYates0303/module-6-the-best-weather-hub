9113ef45378a1034b500a649d53e5423f2325a5e
BucsFan08 <thomasyates16@gmail.com>
2021-11-28 15:16:34 -0500

    added render city function

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,17 @@ var cityname;
 // local storage functions
 initCityList();
 initWeather();
+
+// This function displays the city entered by the user into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
